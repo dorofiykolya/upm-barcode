e3b853481bfd4f07c82537b9f0a6816f768f6ee9
dorofiykolya <dorofiy.kolya@gmail.com>
2023-04-09 19:16:47 +0300

    0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+Barcode decoder for Unity.
+
+## [0.0.1]
+### Make it work
+- Add basic functionality
+- Add package.json
---
+++ Runtime/IWebCamera.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dorofii.Barcode.Runtime
+{
+    public interface IWebCamera : IDisposable
+    {
+        /// <summary>
+        /// It returns the current texture of the camera
+        /// </summary>
+        WebCamTexture Texture { get; }
+
+        /// <summary>
+        /// It returns a snapshot of the current frame by RawImage UVRect
+        /// </summary>
+        /// <param name="rawImage"></param>
+        /// <returns></returns>
+        Texture2D Snapshot(RawImage rawImage);
+
+        /// <summary>
+        /// It sets texture to the RawImage and applies UVRect
+        /// </summary>
+        /// <param name="rawImage"></param>
+        IDisposable Bind(RawImage rawImage);
+
+        /// <summary>
+        /// It tries to decode until it finds a barcode or cancellation token is cancelled
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<DecodeResult> DecodeAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// It tries to decode until it finds a barcode or cancellation token is cancelled, but uses only a part of the image with the given rect
+        /// </summary>
+        /// <param name="rawImage"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<DecodeResult> DecodeAsync(RawImage rawImage, CancellationToken cancellationToken = default);
+    }
+}
